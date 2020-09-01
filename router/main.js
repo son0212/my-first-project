c4ac46a1807b63647285605e081c318d7c65c50b
son0212 <66562261+son0212@users.noreply.github.com>
2020-09-01 13:19:28 +0700

    Update main.js

--- router/main.js
+++ router/main.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
 
 const listSessionId = require('../models/model.sessionId.js');
 
 router.get('/',async (req,res)=>{
-	var mySessionId = await listSessionId.findOne({id:req.cookies.sessionId});
+	var data = await jwt.verify(req.cookies.sessionId,process.env.jwt);
+	var mySessionId = await listSessionId.findOne(data});
 	res.render('index',{information:mySessionId});
 });
 
 router.get('/one',async (req,res)=>{
-	var mySessionId = await listSessionId.findOne({id:req.cookies.sessionId});
+	var data = await jwt.verify(req.cookies.sessionId,process.env.jwt);
+	var mySessionId = await listSessionId.findOne({id:data});
 	res.render('one',{information:mySessionId});
 });
 
