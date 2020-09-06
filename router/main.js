2787a927a81458007534e8ccfd58d36775c6db76
son0212 <66562261+son0212@users.noreply.github.com>
2020-09-07 05:00:04 +0700

    Update main.js

--- router/main.js
+++ router/main.js
@@ -5,12 +5,18 @@ require('dotenv').config();
 const listSessionId = require('../models/model.sessionId.js');
 
 router.get('/',async (req,res)=>{
+	if(!req.cookies.sessionId){
+		res.redirect('/');
+	}
 	var data = await jwt.verify(req.cookies.sessionId,process.env.jwt);
 	var mySessionId = await listSessionId.findOne(data});
 	res.render('index',{information:mySessionId});
 });
 
 router.get('/one',async (req,res)=>{
+	if(!req.cookies.sessionId){
+		res.redirect('/one');
+	}
 	var data = await jwt.verify(req.cookies.sessionId,process.env.jwt);
 	var mySessionId = await listSessionId.findOne({id:data});
 	res.render('one',{information:mySessionId});
