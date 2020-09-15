a7e6f67674044b38413d234a8c85843b518d3d14
son0212 <66562261+son0212@users.noreply.github.com>
2020-09-15 20:45:23 +0700

    Update auth.sessionId.js

--- auth/auth.sessionId.js
+++ auth/auth.sessionId.js
@@ -4,27 +4,27 @@ require('dotenv').config();
 
 module.exports = async (req,res,next)=>{
 	if(!req.cookies.sessionId){
-		var sessionId = await listSessionId.findOne({
-			ip:req.ip,
+		const sessionId = await listSessionId.findOne({
+			ip:req.headers['x-forwarded-for'],
 			os:req.useragent.os,
 			browser:req.useragent.browser
 		});
 		if(sessionId){
-			var token = await jwt.sign({ip:sessionId.ip,id:sessionId._id,os:sessionId.os,browser:sessionId.browser},process.env.jwt,{algorithm:"HS256"});
+			const token = await jwt.sign({ip:sessionId.ip,id:sessionId._id,os:sessionId.os,browser:sessionId.browser},process.env.jwt,{algorithm:"HS256"});
 			res.cookie('sessionId',token);
 		}
 		else{
 			await listSessionId.create({
-				ip:req.ip,
+				ip:req.headers['x-forwarded-for'],
 				os:req.useragent.os,
 				browser:req.useragent.browser
 			});
-			var mySessionId = await listSessionId.findOne({
-				ip:req.ip,
+			const mySessionId = await listSessionId.findOne({
+				ip:req.headers['x-forwarded-for'],
 				os:req.useragent.os,
 				browser:req.useragent.browser
 			});
-			var token = await jwt.sign({ip:mySessionId.ip,id:mySessionId._id,os:mySessionId.os,browser:mySessionId.browser},process.env.jwt,{algorithm:"HS256"});
+			const token = await jwt.sign({ip:mySessionId.ip,id:mySessionId._id,os:mySessionId.os,browser:mySessionId.browser},process.env.jwt,{algorithm:"HS256"});
 			res.cookie('sessionId',token);
 		}
 	}
